00a98136ac4bbdd1669a904b12a77cf2ebf8698e
kals-slak <sundaram.kalyan369@gmail.com>
2024-06-22 16:18:55 +0530

    initial commit

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,8 +1,18 @@
 import server from "./server";
+import * as secp from 'ethereum-cryptography/secp256k1';
+import { keccak256 } from 'ethereum-cryptography/keccak';
+import {toHex} from 'ethereum-cryptography/utils';
+import ethLogo from './img/eth.png'
 
-function Wallet({ address, setAddress, balance, setBalance }) {
+function Wallet({ address, setAddress, balance, setBalance, privateKey, setPrivateKey }) {
   async function onChange(evt) {
-    const address = evt.target.value;
+    const privateKey = evt.target.value;
+    setPrivateKey(privateKey);
+    const publicKey = secp.getPublicKey(privateKey);
+    const hashedPublicKey = keccak256(publicKey.slice(1));
+    const ethereumPublicKey = hashedPublicKey.slice(-20);
+    const address = toHex(ethereumPublicKey);
+
     setAddress(address);
     if (address) {
       const {
@@ -16,13 +26,13 @@ function Wallet({ address, setAddress, balance, setBalance }) {
 
   return (
     <div className="container wallet">
+      <img  src={ethLogo} style={{width: 100, height: 150}} alt="ETH Logo"/>
       <h1>Your Wallet</h1>
-
       <label>
-        Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
+        Private Key
+        <input placeholder="Enter your Private Key" value={privateKey} onChange={onChange}></input>
       </label>
-
+      <div className="balance">ETH-Address: {address}</div>
       <div className="balance">Balance: {balance}</div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -3,13 +3,14 @@ const app = express();
 const cors = require("cors");
 const port = 3042;
 
+
 app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "4f9eb36b1b2f3dc7ee3b8a5a0f5aa06bd5168940": 100,
+  "2742eea4905e7d2276f7790b386d59d629125d0d": 50,
+  "a3ca8cf814227b3319f3dcb200630dc6d26f78f3": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -19,6 +20,9 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
+  // TODO get a signiture from the client side application
+  // TODO recover the public adress from the signiture.
+
   const { sender, recipient, amount } = req.body;
 
   setInitialBalance(sender);
